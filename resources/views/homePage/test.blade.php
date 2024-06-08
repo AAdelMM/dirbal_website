6f94f6ee9ec1347782b520ab767447b67c572a8d
AAdelMM <ahmed.adel426@gmail.com>
2024-06-08 23:55:17 +0300

    test small

--- resources/views/homePage/test.blade.php
+++ resources/views/homePage/test.blade.php
@@ -136,7 +136,7 @@
       <!--content-->
       <div class="content absolute">
       <div class="container flex justify-center w-[67.6vw] h-[70vh]  relative">
-  <div class="Lastframecontent flex justify-center absolute left-0 top-[30%] ">
+  <div class="Lastframecontent flex justify-center absolute left-0 top-[25vh] ">
       <!--topics-->
     <div class="Group94 w-[31.5vw] h-[34.8vh] left-[1.8vw] top-[0.25em] absolute">
           <div class="DivMkdItemTextHolder w-[31.5vw] h-[12.6vh] left-0 top-0 absolute bg-white border border-zinc-300">
@@ -192,21 +192,21 @@
           
     </div>
     <div class="Frame320 w-[31.5vw] left-[69.5vh] top-[2.2vh] absolute justify-end items-center gap-[1.3em] inline-flex">
-      <div class=" w-[27.5vw] text-right text-orange-400 text-[1.5em] font-bold font-['EL Meesiri'] ">69 م د: فسخ عقد مبرم بين الغير ( عدم قبول لعدم المصلحة )‎</div>
+      <div class=" w-[27.5vw] text-right text-orange-400  2xl:text-[1.5em] xl:text-[1.2em] font-bold font-['EL Meesiri'] ">69 م د: فسخ عقد مبرم بين الغير ( عدم قبول لعدم المصلحة )‎</div>
       <div class="Frame309 w-[2.9vw] h-[5.1vh] flex-col justify-start items-center inline-flex">
         <div class="Group49 w-[2.9vw] h-[5.1vh] relative">
           <img class="Regulation w-[2.9vw] h-[5.1vh] left-0 top-0 absolute" src="https://via.placeholder.com/55x55" />
           <div class="Regulation w-[2.9vw] h-[5.1vh] left-0 top-0 absolute bg-orange-400"></div>
         </div>
-        <div class=" w-[3.9vw] h-[2.1vh] left-[27.8vw] top-[8vh] absolute text-center text-white text-sm font-normal font-['Arial'] capitalize">القضاء المدني</div>
+        <div class=" w-[3.9vw] h-[2.1vh] left-[27.8vw] top-[8vh] absolute text-center text-white 2xl:text-sm xl:text-[0.65em] font-normal font-['Arial'] capitalize">القضاء المدني</div>
       </div>
     </div>
-    <div class="Line43 w-[30.9vw] h-[0px] left-[66.6vw] top-[13vh] absolute origin-top-left rotate-[180deg] shadow border border-stone-400/opacity-50"></div>
+    <div class="Line43 w-[30.9vw] h-[0px] left-[66.6vw] top-[14vh] absolute origin-top-left rotate-[180deg] shadow border border-stone-400/opacity-50"></div>
     <div class="BtnKeepRead w-11 h-[13.8vh] left-[58.2vw] top-[32.8vh] absolute origin-top-left -rotate-90 bg-orange-400 rounded-[3px]">
-      <div class="Frame273 left-[1.7vw] top-[3.5vh] absolute flex-col justify-center items-center gap-2 inline-flex">
-        <div class=" w-[5.2vw] h-[2.4vh] origin-top-left rotate-90 text-center text-white text-[0.9em] font-bold font-['El Messiri'] ">واصل القراءة</div>
+      <div class="Frame273 left-[1.7vw] 2xl:top-[3.5vh] xl:top-[2.5vh] absolute flex-col justify-center items-center gap-2 inline-flex">
+        <div class=" w-[5.2vw] h-[2.4vh] xl:ml-[0.9em]  origin-top-left rotate-90 text-center text-white 2xl:text-[0.9em] xl:text-[0.75em] font-bold font-['El Messiri'] ">واصل القراءة</div>
       </div>
-      <div class="Btndarken w-[2.2vw] h-11 left-[2.3vw] top-0 absolute origin-top-left rotate-90 opacity-50 bg-orange-400 rounded-tl-[3px] rounded-bl-[3px]"></div>
+      <div class="Btndarken w-[2.2vw] h-11 2xl:left-[2.3vw] xl:left-[3.2vw] top-0 absolute origin-top-left rotate-90 opacity-50 bg-orange-400 rounded-tl-[3px] rounded-bl-[3px]"></div>
     </div>
     <div class="Menu w-[13.2vw] h-[7.9vh] left-[35.1vw] top-[25.3vh] absolute justify-start items-start inline-flex">
       <div class="DivMkdPostInfoDate w-[4.4vw] h-[7.9vh] relative border border-white">
@@ -219,7 +219,7 @@
         <div class="Share left-[1vw] top-[6vh] absolute justify-start items-start gap-2.5 inline-flex"></div>
       </div>
     </div>
-    <div class=" w-[31.3vw] h-[10.6vh] left-[35.1vw] top-[15vh] absolute text-right text-white text-lg font-normal font-['El Messiri'] uppercase leading-normal">المصلحة الشخصية المشروعة المباشرة شرط لقبول الدعوى. مِلاكها أن يكون للمدعي من وراء التجائه إلى القضاء منفعةٌ حقيقة،</div>
+    <div class=" w-[31.3vw] h-[10.6vh] left-[35.1vw] top-[15vh] absolute text-right text-white 2xl:text-[1.4em] xl:text-[1em] font-normal font-['El Messiri'] uppercase leading-normal">المصلحة الشخصية المشروعة المباشرة شرط لقبول الدعوى. مِلاكها أن يكون للمدعي من وراء التجائه إلى القضاء منفعةٌ حقيقة،</div>
     
   </div>
   </div>
