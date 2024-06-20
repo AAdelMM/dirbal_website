c59e03418cc5366da01730a1d206c9eaa868d8c5
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-06-20 16:14:33 +0300

    create judge section

---
+++ resources/views/sections/judgment.blade.php
@@ -0,0 +1,72 @@
+
+<div class="Home w-full lg:h-[100vh] h-[80vh] relative">
+  <img class="sectionBg w-full lg:h-[100vh] h-[80vh] left-0 top-[1px] absolute" src="{{ asset('images/judgbg.png') }}" />
+  <div class="Frame285 w-[68.4vw] h-[39.8vh] left-[23.9vw] top-[7.5rem] absolute " style="direction:rtl;">
+    <div class=" w-[68.4vw]  lg:h-[13vh] h-[10vh] text-right text-white 2xl:text-[8rem] lg:text-[6rem] text-[4rem] font-normal " style="font-family:'Noto Kufi Arabic';">القضاء</div>
+    <div class=" w-[74vw]  h-[24vh] 2xl:mt-[5rem] lg:mt-[2.5rem]  text-white 2xl:text-[2rem] text-[1.5rem]  font-normal 2xl:leading-[4rem] leading-[3rem]" style="font-family:'El Messiri';">القانون الليبي هو المرجع في الأعمال، لكن قدراً غير يسير منها يتوافق مع قوانين دول عربية لاسيما المصري، المصدر التاريخي للقانون الليبي ولكثير من القوانين العربية. تيسيراً للزائر في معرفة مدى تعلق العمل باهتمامه، قدمت له بعرض موجز لأهم القواعد التي يتضمنها ولتلك التي بني عليها. كما أضفت تسجيلاً صوتياً لذلك لمن يؤثر الاستماع على القراءة.</div>
+  </div>
+
+  <div class="SubSection w-full 2xl:h-[21.25rem] h-[12rem] left-0 2xl:top-[38rem] lg:top-[33rem] top-[35rem] flex justify-center absolute bg-gray-700/opacity-20 border-t-2 border-orange-400">
+    <div class="Frame34 w-[86.15vw]   absolute justify-center items-center 2xl:gap-[11rem] lg:gap-[8rem] gap-[4rem] inline-flex">
+
+      <div class="Frame258   flex-col justify-center items-center inline-flex pt-[4rem]">
+        <div class="Group54 w-[7.3vw] lg:h-[8.75rem] h-[6rem] flex justify-center">
+        <img class="Evidence 2xl:w-[7rem] 2xl:h-[7rem] lg:w-[6rem] lg:h-[6rem] w-[4rem] h-[4rem] " src="{{asset('images/jicon1.png')}}" />
+        </div>
+        <div class=" text-gray-200 lg:text-[2rem] text-[1.5rem] font-bold text-center " style="font-family:'Noto Kufi Arabic">معهد القضاء</div>
+        
+      </div>
+      <img src="{{asset('images/vline.png')}}" alt="">
+
+      <div class="Frame260   flex-col justify-center items-center  pt-[4rem] ">
+      <div class="Group54 w-[7.3vw] lg:h-[8.75rem] h-[6rem] flex justify-center">
+        <img class="Evidence 2xl:w-[7rem] 2xl:h-[7rem] lg:w-[6rem] lg:h-[6rem] w-[4rem] h-[4rem]  " src="{{asset('images/jicon2.png')}}" />
+        </div>
+        <div class="text-gray-200 lg:text-[2rem]  text-[1.5rem] font-bold text-center " style="font-family:'Noto Kufi Arabic">المحكمة</div>
+      </div>
+      <img src="{{asset('images/vline.png')}}" alt="">
+
+      <div class="Frame259  flex-col justify-center items-center inline-flex pt-[4rem]">
+        <div class="Group63 w-[7.3vw] lg:h-[8.75rem] h-[6rem] flex justify-center">
+          <img class="Evidence 2xl:w-[7rem] 2xl:h-[7rem] lg:w-[6rem] lg:h-[6rem] w-[4rem] h-[4rem]  " src="{{asset('images/jicon3.png')}}" />
+        </div>
+        <div class=" text-gray-200 lg:text-[2rem] text-[1.5rem] font-bold  text-center" style="font-family:'Noto Kufi Arabic">النيابة العامة</div>
+      </div>
+    </div>
+  </div>
+  <div class="HeaderHome w-[54vw] 2xl:h-[7.8rem] lg:h-[5.5em] h-[4rem] 2xl:left-[60.57px] lg:left-[2.7em] left-[1.5rem] top-0 absolute">
+    <div class="Line w-[89.3vw]  h-[0px] left-[8.72px] 2xl:top-[113px] lg:top-[4.9em] top-[3.2rem] absolute border border-white"></div>
+   
+        
+        <div class="Logo left-0 2xl:top-[14px] lg:top-[0.6em] top-[0.4rem] absolute justify-center items-center 2xl:gap-2.5 lg:gap-1.5 gap-1 inline-flex">
+          <div class="Group47 2xl:w-[14rem] lg:w-[9.8rem] w-[8rem] 2xl:h-[76px] xl:h-[3.3em] relative">
+<!---->
+            <img class="Aaaaaa1 2xl:w-[14rem] lg:w-[9.8rem] w-[8rem] 2xl:h-[76px] lg:h-[3.3em] h-[2.6rem] left-0  top-[0rem] absolute" src="{{ asset('images/48.png') }}" />     
+          </div>
+        </div>
+
+        <div class="Frame43 2xl:left-[77vw] lg:left-[73vw] left-[79vw]  2xl:top-[27px] lg:top-[1.2em] top-[0.6rem] absolute justify-center items-center 2xl:gap-[15px] xl:gap-[1em] inline-flex">
+          <div class="Icons8Bookmark1 2xl:w-[38.64px] lg:w-[1.6em] w-[1.2rem] 2xl:h-[54.21px] lg:h-[2.3em] h-[1.8rem] relative">
+          
+            <div class="Ellipse3 2xl:w-[25px] lg:w-[1.1em] w-[0.8rem] 2xl:h-[25px] lg:h-[1.1em] h-[0.8rem] 2xl:left-[13.64px] lg:left-[0.6em] left-[0.4rem] 2xl:top-0 lg:top-[-0.4em] top-[-0.2rem] absolute bg-red-600 rounded-full"></div>
+            <div class=" 2xl:w-2 lg:w-1 w-0.5 lg:h-[19px] h-[15px] 2xl:left-[20.5px] lg:left-[1em] left-[0.5rem] 2xl:top-[-3px] lg:top-[-0.6em] top-[-0.4rem] absolute  text-white 2xl:text-[1.25em] lg:text-[0.9em] text-[0.6rem] font-normal " style="font-family:'Arial';">3</div>  
+         
+        </div>
+          
+          <!-- <img class=" favorite cursor-pointer" src="{{ asset('images/fav.png') }}" alt="fav"> -->
+          <img class="favorite cursor-pointer" src="{{ asset('images/favw.png') }}" alt="fav" onclick="redirectToFavorite()">
+          <div class="SpanMkdVerticalSeparator w-px 2xl:h-12 lg:h-10 h-8 border-r border-white"></div>
+          <div class="Icons8Search11 2xl:w-10 lg:w-8 w-6 2xl:h-10 lg:h-8 h-6 relative"></div>
+          <img class="searchIcon cursor-pointer" src="{{ asset('images/magniw.png') }}" alt="search">
+          <div class="SpanMkdVerticalSeparator w-px 2xl:h-12 lg:h-10 h-8 border-r border-white"></div>
+          <div class="Icons8Menu11 2xl:w-10 lg:w-8 w-6 2xl:h-10 lg:h-8 h-6  relative"></div>
+          <img id="menuIcon" class="menuIcon cursor-pointer" src="{{ asset('images/menuw.png') }}" alt="menu">
+          <div class="SpanMkdVerticalSeparator w-px 2xl:h-12 lg:h-10 h-8 border-r border-white"></div>
+          
+        </div>
+      </div>
+  <div class="SwitchRtf w-[1920px] h-[60px] left-0 top-[438px] absolute">
+    <div class="AngleLeft w-[60px] h-[60px] left-[60px] top-[60px] absolute origin-top-left -rotate-180 bg-zinc-300/opacity-10"></div>
+  </div>
+</div>
+
