4d8ff21e297f1f1a83461245b7f8ff51b351ba69
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-07-03 08:57:43 +0300

    last topics edit

--- resources/views/homePage/lastTopics.blade.php
+++ resources/views/homePage/lastTopics.blade.php
@@ -4,7 +4,7 @@
       <div class="wave wave2"></div>
     </div>
       <!--title-->
-   <div  class="LastTopics flex justify-center relative bg-gray-800">
+   <div  class="LastTopics flex justify-center items-center relative bg-gray-800">
       <div class="subtitle-container flex justify-center w-[72vw] h-[6.4vh] mt-[1.4em] ">
          
           <div id="subtitle" class="   text-right text-white 2xl:text-[1em] xl:text-[0.7em] font-normal font-['El Messiri'] ">آخر الموضوعات </div>  
@@ -12,93 +12,111 @@
                   <img class="w-[2em] h-auto right-[15vw] absolute " src="{{ asset('images/IocndecorR.svg') }}" alt="icon">   
           </div>
       <!--content-->
-      <div class="content absolute">
-      <div class="container flex justify-center w-[67.6vw] h-[70vh]  relative">
-  <div class="z-10 Lastframecontent flex justify-center absolute left-0 top-[25vh] ">
-      <!--topics-->
-    <div class="Group94 w-[31.5vw] h-[34.8vh] left-[1.8vw] top-[0.25em] absolute">
-          <div class="DivMkdItemTextHolder w-[31.5vw] h-[12.6vh] left-0 top-0 absolute bg-white border border-zinc-300">
-                  <div class=" w-[26.5vw] left-[4.2vw] top-[0.8em] absolute text-justify text-gray-700 2xl:text-[1.6em] xl:text-[1.2em] font-normal font-['Amiri'] ">69 م د: فسخ عقد مبرم بين الغير (عدم قبول لعدم المصلحة)‎</div>
-                  <div class="Frame275 w-[10vw] left-[23vw] top-[7.8vh] absolute justify-center items-center 2xl:gap-1.5 xl:gap-1 inline-flex">
-                        <div class=" text-orange-400 2xl:text-xl xl:text-lg font-bold font-['Amiri'] ">القضاء المدني‎</div>
-                        <div class="Frame w-[0.7vw] h-[1.3vh] relative"></div>
-                  </div>
-              
-            <div class="Group93 w-[3.1vw] h-[7.2vh] left-[-1.9em] top-[1.1em] absolute">
-                      <div class="SpanMkdPostInfoDateDay w-[3.1vw] h-[5.7vh] left-0 2xl:top-0 xl:top-[-0.5vh] absolute bg-gray-700">
-                        <div class=" w-[1.5vw] h-[2.7vh] left-[0.8vw] top-0 absolute text-center text-white 2xl:text-[1.5em] xl:text-[1.1em] font-black font-['Roboto'] leading-[50px]">18</div>
+      <div class="content  flex items-center z-[10] ">
+      <div class="container  inline-flex justify-between 2xl:w-[68vw] w-[84vw] h-[70vh] mt-[10vh] items-center relative">
+
+         
+          <!--topics-->
+           <div class="Group94 flex 2xl:w-[31.5vw] w-[37vw] h-[34.8vh] relative ">
+              <div class="DivMkdItemTextHolder 2xl:w-[31.5vw] w-[37vw] h-[12.6vh] flex justify-center items-center left-0 top-0 absolute bg-white border border-zinc-300">
+                      <div class=" 2xl:w-[26.5vw] w-[30vw]  left-[4.2vw] top-[0.8em] absolute text-justify text-gray-700 2xl:text-[1.6em] xl:text-[1.2em] font-normal  " style="font-family:'El Messiri'; direction:rtl;">69 م د: فسخ عقد مبرم بين الغير (عدم قبول لعدم المصلحة)‎</div>
+                      <div class="Frame275 w-[30vw] 2xl:w-[25vw] top-[7.8vh] absolute justify-start items-end 2xl:gap-1.5 xl:gap-1 inline-flex">
+                            <div class=" text-orange-400 2xl:text-xl xl:text-lg font-bold font-['Amiri'] ">القضاء المدني‎</div>
+                            <div class="Frame w-[0.7vw] h-[1.3vh] relative"></div>
                       </div>
-                      <div class="SpanMkdPostInfoDateMonth w-[3.1vw] h-[3.8vh] left-0 top-[5vh] absolute bg-orange-400">
-                        <div class=" left-[0.9vw] top-[-0.25em] absolute text-center text-white 2xl:text-[1.4em] xl:text-[1em] font-normal font-['Arial'] leading-loose">08</div>
-                      </div>
+                  
+                <div class="Group93 w-[3.1vw] h-[7.2vh] left-[-1.9em] top-[1.1em] absolute">
+                          <div class="SpanMkdPostInfoDateDay w-[3.1vw] h-[5.7vh] left-0 2xl:top-0 xl:top-[-0.5vh] absolute bg-gray-700">
+                            <div class=" w-[1.5vw] h-[2.7vh] left-[0.8vw] top-0 absolute text-center text-white 2xl:text-[1.5em] xl:text-[1.1em] font-black font-['Roboto'] leading-[50px]">18</div>
+                          </div>
+                          <div class="SpanMkdPostInfoDateMonth w-[3.1vw] h-[3.8vh] left-0 top-[5vh] absolute bg-orange-400">
+                            <div class=" left-[0.9vw] top-[-0.25em] absolute text-center text-white 2xl:text-[1.4em] xl:text-[1em] font-normal font-['Arial'] leading-loose">08</div>
+                          </div>
+                </div>
             </div>
-         </div>
-         <!--topic2-->
-         <div class="DivMkdItemTextHolder w-[31.5vw] h-[12.6vh] left-0 top-[13vh] absolute bg-white border border-zinc-300">
-                  <div class=" w-[26.5vw] left-[4.2vw] top-[0.8em] absolute text-justify text-gray-700 2xl:text-[1.6em] xl:text-[1.2em] font-normal font-['Amiri'] ">69 م د: فسخ عقد مبرم بين الغير (عدم قبول لعدم المصلحة)‎</div>
-                  <div class="Frame275 w-[10vw] left-[23vw] top-[7.8vh] absolute justify-center items-center 2xl:gap-1.5 xl:gap-1.2 inline-flex">
-                        <div class=" text-orange-400 2xl:text-xl xl:text-lg font-bold font-['Amiri'] ">القضاء المدني‎</div>
-                        <div class="Frame w-[0.7vw] h-[1.3] relative"></div>
-                  </div>
-              
-            <div class="Group93 w-[3.1vw] h-[7.2vh] left-[-1.9em] top-[1.1em] absolute">
-                      <div class="SpanMkdPostInfoDateDay w-[3.1vw] h-[5.7vh] left-0 2xl:top-0 xl:top-[-0.5vh] absolute bg-gray-700">
-                        <div class=" w-[1.5vw] h-[2.7vh] left-[0.8vw] top-0 absolute text-center text-white 2xl:text-[1.5em] xl:text-[1.1em] font-black font-['Roboto'] leading-[50px]">18</div>
+            <!--topic2-->
+            <div class="DivMkdItemTextHolder 2xl:w-[31.5vw] w-[37vw] h-[12.6vh] flex justify-center left-0 top-[13vh] absolute bg-white border border-zinc-300">
+                      <div class=" 2xl:w-[26.5vw] w-[30vw]  left-[4.2vw] top-[0.8em] absolute text-justify text-gray-700 2xl:text-[1.6em] xl:text-[1.2em] font-normal  "style="font-family:'El Messiri';direction:rtl;">69 م د: فسخ عقد مبرم بين الغير (عدم قبول لعدم المصلحة)‎</div>
+                      <div class="Frame275 w-[30vw] 2xl:w-[25vw] top-[7.8vh] absolute justify-start items-center 2xl:gap-1.5 xl:gap-1.2 inline-flex">
+                            <div class=" text-orange-400 2xl:text-xl xl:text-lg font-bold font-['Amiri'] ">القضاء المدني‎</div>
+                            <div class="Frame w-[0.7vw] h-[1.3] relative"></div>
                       </div>
-                      <div class="SpanMkdPostInfoDateMonth w-[3.1vw] h-[3.8vh] left-0 top-[5vh] absolute bg-orange-400">
-                        <div class=" left-[0.9vw] top-[-0.25em] absolute text-center text-white 2xl:text-[1.4em] xl:text-[1em] font-normal font-['Arial'] leading-loose">08</div>
-                      </div>
+                  
+                <div class="Group93 w-[3.1vw] h-[7.2vh] left-[-1.9em] top-[1.1em] absolute">
+                          <div class="SpanMkdPostInfoDateDay w-[3.1vw] h-[5.7vh] left-0 2xl:top-0 xl:top-[-0.5vh] absolute bg-gray-700">
+                            <div class=" w-[1.5vw] h-[2.7vh] left-[0.8vw] top-0 absolute text-center text-white 2xl:text-[1.5em] xl:text-[1.1em] font-black font-['Roboto'] leading-[50px]">18</div>
+                          </div>
+                          <div class="SpanMkdPostInfoDateMonth w-[3.1vw] h-[3.8vh] left-0 top-[5vh] absolute bg-orange-400">
+                            <div class=" left-[0.9vw] top-[-0.25em] absolute text-center text-white 2xl:text-[1.4em] xl:text-[1em] font-normal font-['Arial'] leading-loose">08</div>
+                          </div>
+                </div>
             </div>
-         </div>
-        <!--topic3-->
-         <div class="DivMkdItemTextHolder w-[31.5vw] h-[12.6vh] left-0 top-[25.9vh] absolute bg-white border border-zinc-300">
-                  <div class=" w-[26.5vw] left-[4.2vw] top-[0.8em] absolute text-justify text-gray-700 2xl:text-[1.6em] xl:text-[1.2em] font-normal font-['Amiri'] ">69 م د: فسخ عقد مبرم بين الغير (عدم قبول لعدم المصلحة)‎</div>
-                  <div class="Frame275 w-[10vw] left-[23vw] top-[7.8vh] absolute justify-center items-center 2xl:gap-1.5 xl:gap-1.2 inline-flex">
-                        <div class=" text-orange-400 2xl:text-xl xl:text-lg font-bold font-['Amiri'] ">القضاء المدني‎</div>
-                        <div class="Frame w-[0.7vw] h-[1.3] relative"></div>
-                  </div>
-              
-            <div class="Group93 w-[3.1vw] h-[7.2vh] left-[-1.9em] top-[1.1em] absolute">
-                      <div class="SpanMkdPostInfoDateDay w-[3.1vw] h-[5.7vh]  left-0 2xl:top-0 xl:top-[-0.5vh] absolute bg-gray-700">
-                        <div class=" w-[1.5vw] h-[2.7vh] left-[0.8vw] top-0 absolute text-center text-white 2xl:text-[1.5em] xl:text-[1.1em] font-black font-['Roboto'] leading-[50px]">18</div>
+            <!--topic3-->
+            <div class="DivMkdItemTextHolder 2xl:w-[31.5vw] w-[37vw] h-[12.6vh] flex justify-center left-0 top-[25.9vh] absolute bg-white border border-zinc-300">
+                      <div class=" 2xl:w-[26.5vw] w-[30vw] left-[4.2vw] top-[0.8em] absolute text-justify text-gray-700 2xl:text-[1.6em] xl:text-[1.2em] font-normal " style="font-family:'El Messiri';direction:rtl;">69 م د: فسخ عقد مبرم بين الغير (عدم قبول لعدم المصلحة)‎</div>
+                      <div class="Frame275 w-[30vw] 2xl:w-[25vw]  top-[7.8vh] absolute justify-start items-center 2xl:gap-1.5 xl:gap-1.2 inline-flex">
+                            <div class=" text-orange-400 2xl:text-xl xl:text-lg font-bold font-['Amiri'] ">القضاء المدني‎</div>
+                            <div class="Frame w-[0.7vw] h-[1.3] relative"></div>
                       </div>
-                      <div class="SpanMkdPostInfoDateMonth w-[3.1vw] h-[3.8vh] left-0 top-[5vh]  absolute bg-orange-400">
-                        <div class=" left-[0.9vw] top-[-0.25em] absolute text-center text-white 2xl:text-[1.4em] xl:text-[1em] font-normal font-['Arial'] leading-loose">08</div>
-                      </div>
+                  
+                <div class="Group93 w-[3.1vw] h-[7.2vh] left-[-1.9em] top-[1.1em] absolute">
+                          <div class="SpanMkdPostInfoDateDay w-[3.1vw] h-[5.7vh]  left-0 2xl:top-0 xl:top-[-0.5vh] absolute bg-gray-700">
+                            <div class=" w-[1.5vw] h-[2.7vh] left-[0.8vw] top-0 absolute text-center text-white 2xl:text-[1.5em] xl:text-[1.1em] font-black font-['Roboto'] leading-[50px]">18</div>
+                          </div>
+                          <div class="SpanMkdPostInfoDateMonth w-[3.1vw] h-[3.8vh] left-0 top-[5vh]  absolute bg-orange-400">
+                            <div class=" left-[0.9vw] top-[-0.25em] absolute text-center text-white 2xl:text-[1.4em] xl:text-[1em] font-normal font-['Arial'] leading-loose">08</div>
+                          </div>
+                </div>
             </div>
-         </div>
-          
-    </div>
-    <div class="Frame320 w-[31.5vw] 2xl:left-[69.5vh] xl:left-[70.5vh] top-[2.2vh] absolute justify-end items-center gap-[1.3em] inline-flex">
-      <div class=" w-[27.5vw] text-right text-orange-400  2xl:text-[1.5em] xl:text-[1.2em] font-bold font-['EL Meesiri'] ">69 م د: فسخ عقد مبرم بين الغير ( عدم قبول لعدم المصلحة )‎</div>
-      <div class="Frame309 w-[2.9vw] h-[5.1vh] flex-col justify-start items-center inline-flex">
-        <div class="Group49 w-[2.9vw] h-[5.1vh] relative">
-          <img class="Regulation w-[2.9vw] h-[5.1vh] left-0 top-0 absolute" src="https://via.placeholder.com/55x55" />
-          <div class="Regulation w-[2.9vw] h-[5.1vh] left-0 top-0 absolute bg-orange-400"></div>
+              
+            </div>
+    <!--col2-->
+    <div class=" flex flex-col 2xl:w-[33vw] w-[40vw] h-[34.8vh] justify-end items-center relative">
+    <div class="  flex 2xl:w-[33vw] w-[40vw] h-[34.8vh] justify-between items-center ">
+            <!--col title -->
+            <div class="Frame320 2xl:w-[33vw] w-[40vw]  top-[2.2vh] 2xl:left-0  absolute justify-end items-center gap-[1.3em] inline-flex">
+            <div class=" w-[30vw] text-right text-[#CF9455]  2xl:text-[1.6em] text-[1.3em] font-bold  " style="font-family:'Noto Kufi Arabic';">69 م د: فسخ عقد مبرم بين الغير ( عدم قبول لعدم المصلحة )‎</div>
+              <div class="Frame309 w-[2.9vw] h-[5.1vh] flex-col justify-start items-center inline-flex">
+                      <div class="Group49 w-[2.9vw] h-[5.1vh] flex flex-col justify-center relative">
+                        <img class="Regulation w-[2.9vw] h-[5.1vh] left-0 top-0 absolute" src="https://via.placeholder.com/55x55" />
+                        <div class="Regulation w-[2.9vw] h-[5.1vh] left-0 top-0 absolute bg-[#CF9455]"></div>
+                        <div class=" w-[4vw] h-[2.1vh] left-[-0.5vw] top-[8vh] absolute text-center text-white 2xl:text-sm xl:text-[0.65em] font-normal font-['Arial'] capitalize">القضاء المدني</div>
+
+                      </div>
+              </div>
+          </div>
+          <!--end col title-->
+          <div class="flex w-[33vw] h-[30vh] absolute">
+          <div class="flex w-[33vw] h-[30vh]  relative">
+               <div class="Line43 2xl:w-[33vw] w-[40vw] h-[0px] 2xl:left-[33vw] left-[39.9vw] top-[14vh] absolute origin-top-left rotate-[180deg] shadow border border-stone-400/opacity-50"></div>
+
+            <div class="BtnKeepRead w-[7vw] h-[5vh] 2xl:left-[26.5vw] left-[32vw] top-[30vh] relative  bg-[#CF9455] rounded-[3px] inline-flex">
+                  <div class="Frame273  top-[2.5vh]   justify-center items-center gap-2 inline-flex">
+                    <div class=" w-[5.2vw] h-[2.4vh] xl:ml-[0.9em]   text-center text-white 2xl:text-[0.9em] xl:text-[0.75em] font-bold" style="font-family:'El Messiri'">واصل القراءة</div>
+                  </div>
+
+            </div>
+
+                <div class="Menu w-[13.2vw] h-[7.9vh] top-[27vh] absolute justify-start items-start inline-flex">
+                    <div class="DivMkdPostInfoDate w-[4.4vw] h-[7.9vh] relative border border-white">
+                    <div class="Copy  left-[1.2vw] top-[1.8vh] h-[7.9vh] justify-center items-center flex "><img class="w-[2.5vw]  h-[5vh]" src="{{asset('images/COPY.png')}}" alt=""></div>
+                </div>
+
+            <div class="DivMkdPostInfoCommentsHolder w-[4.4vw] h-[7.9vh] relative border border-gray-200">
+              <div class="Comment  left-[0.9vw] top-[1.6vh] h-[7.9vh]   justify-center items-center  flex"><img class="w-[2.5vw]  h-[5vh]" src="{{asset('images/comment.png')}}" alt=""></div>
+            </div>
+
+            <div class="AMkdSocialShareDropdownOpener w-[4.4vw] h-[7.9vh] relative border border-white">
+              <div class="Share left-[1vw] top-[6vh] h-[7.9vh] justify-center items-center flex"><img class="w-[2.5vw]  h-[5vh]" src="{{asset('images/SHARE.png')}}" alt=""></div>
+            </div>
+
+          </div>
+          <div class=" 2xl:w-[33vw] w-[40vw] h-[10.6vh]  top-[16vh] absolute text-right text-white 2xl:text-[1.4em] xl:text-[1em] font-normal uppercase leading-normal" style="direction:rtl; font-family:'El Messiri';">المصلحة الشخصية المشروعة المباشرة شرط لقبول الدعوى. مِلاكها أن يكون للمدعي من وراء التجائه إلى القضاء منفعةٌ حقيقة،</div>
+          </div>
         </div>
-        <div class=" w-[3.9vw] h-[2.1vh] left-[27.8vw] top-[8vh] absolute text-center text-white 2xl:text-sm xl:text-[0.65em] font-normal font-['Arial'] capitalize">القضاء المدني</div>
-      </div>
+        </div>
     </div>
-    <div class="Line43 w-[30.9vw] h-[0px] left-[66.6vw] top-[14vh] absolute origin-top-left rotate-[180deg] shadow border border-stone-400/opacity-50"></div>
-    <div class="BtnKeepRead w-11 h-[13.8vh] left-[58.2vw] top-[32.8vh] absolute origin-top-left -rotate-90 bg-orange-400 rounded-[3px]">
-      <div class="Frame273 left-[1.7vw] 2xl:top-[3.5vh] xl:top-[2.5vh] absolute flex-col justify-center items-center gap-2 inline-flex">
-        <div class=" w-[5.2vw] h-[2.4vh] xl:ml-[0.9em]  origin-top-left rotate-90 text-center text-white 2xl:text-[0.9em] xl:text-[0.75em] font-bold font-['El Messiri'] ">واصل القراءة</div>
-      </div>
-      <div class="Btndarken w-[2.2vw] h-11 2xl:left-[2.3vw] xl:left-[3.2vw] top-0 absolute origin-top-left rotate-90 opacity-50 bg-orange-400 rounded-tl-[3px] rounded-bl-[3px]"></div>
-    </div>
-    <div class="Menu w-[13.2vw] h-[7.9vh] left-[35.1vw] top-[25.3vh] absolute justify-start items-start inline-flex">
-      <div class="DivMkdPostInfoDate w-[4.4vw] h-[7.9vh] relative border border-white">
-        <div class="Copy left-[1.2vw] top-[1.8vh] absolute justify-start items-start gap-2.5 inline-flex"></div>
-      </div>
-      <div class="DivMkdPostInfoCommentsHolder w-[4.4vw] h-[7.9vh] relative border border-gray-200">
-        <div class="Comment py-0.5 left-[0.9vw] top-[1.6vh] absolute bg-black/opacity-0 flex-col justify-start items-start gap-2.5 inline-flex"></div>
-      </div>
-      <div class="AMkdSocialShareDropdownOpener w-[4.4vw] h-[7.9vh] relative border border-white">
-        <div class="Share left-[1vw] top-[6vh] absolute justify-start items-start gap-2.5 inline-flex"></div>
-      </div>
-    </div>
-    <div class=" w-[31.3vw] h-[10.6vh] left-[35.1vw] top-[15vh] absolute text-right text-white 2xl:text-[1.4em] xl:text-[1em] font-normal font-['El Messiri'] uppercase leading-normal">المصلحة الشخصية المشروعة المباشرة شرط لقبول الدعوى. مِلاكها أن يكون للمدعي من وراء التجائه إلى القضاء منفعةٌ حقيقة،</div>
-    
+
   </div>
   </div>
   </div>
