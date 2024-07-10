01ee6a89ffba45b009a536246aaf31f97d2feb31
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-07-10 23:16:05 +0300

    finalise last topics with choices sections

--- resources/views/homePage/lastTopics.blade.php
+++ resources/views/homePage/lastTopics.blade.php
@@ -1,5 +1,5 @@
 <section id="section-2"  class="relative">
-        <div class="container-fluid w-[100vw] flex justify-center mt-3  absolute">
+        <div class="container-fluid w-[100vw] flex justify-center mt-9  absolute">
                 <div class="lasttopicsIcon inline-flex justify-center items-center top-0  z-20">
                       <img src="{{asset('images/hline.png')}}" alt="decoration line">
                       <img class="w-[3rem] h-[3rem] mx-6 " src="{{asset('images/lasttopicsIcon.png')}}" alt="main icon">
@@ -13,9 +13,9 @@
     </div>
    
       <!--title-->
-   <div  class="LastTopics  2xl:h-[75.8vh] h-[90vh]  flex justify-center items-center relative bg-gray-800">
+   <div  class="LastTopics  2xl:h-[100vh] h-[100vh] pt-[5rem] flex justify-center items-center relative bg-gray-800">
     
-      <div class="subtitle-container inline-flex items-center justify-between w-[64vw] h-[6.4vh] mt-[1em] ">
+      <div class="subtitle-container inline-flex items-center justify-between w-[64vw] h-[6.4vh] 2xl:mt-[3em] mt-[2em] ">
                   <img class="w-[1em] h-auto " src="{{ asset('images/IocndecorL.svg') }}" alt="icon">
                   <div class="flex flex-col items-center justify-center 2xl:mt-[1.7vw] mt-[2.5vw]">
                       <div id="title-row1" class="   text-right text-[#CF9455] 2xl:text-[1em] xl:text-[0.7em] font-normal pb-5 font-['El Messiri'] "> آخرُ الموضوعات المرفوعة<span class="text-white"><span>  </span>إلى مختلف الفروع</span> </div>  
@@ -86,7 +86,7 @@
     <div class=" flex flex-col 2xl:w-[33vw] w-[40vw] h-[34.8vh] justify-end items-center relative">
     <div class="  flex 2xl:w-[33vw] w-[40vw] h-[34.8vh] justify-between items-center ">
             <!--col title -->
-            <div class="Frame320 2xl:w-[33vw] w-[40vw]  top-[2.2vh] 2xl:left-0  absolute justify-end items-center gap-[1.3em] inline-flex">
+            <div class="Frame320 2xl:w-[33vw] w-[40vw]  top-[0.2vh] 2xl:left-0  absolute justify-end items-center gap-[1.3em] inline-flex">
             <div class=" w-[31vw] text-right text-[#CF9455]  2xl:text-[1.5em] text-[1.2em] font-bold  " style="font-family:'Noto Kufi Arabic';direction:rtl;">69 م د: فسخ عقد مبرم بين الغير ( عدم قبول لعدم المصلحة )‎</div>
               <div class="Frame309 w-[2.9vw] h-[5.1vh] flex-col justify-start items-center inline-flex">
                       <div class="Group49 w-[2.9vw] h-[auto] flex flex-col justify-center relative">
@@ -98,22 +98,22 @@
               </div>
           </div>
           <!--end col title-->
-          <div class="flex w-[33vw] h-[30vh] absolute">
+          <div class="flex w-[33vw] h-[30vh] top-[-0.1vh] absolute">
           <div class="flex w-[33vw] h-[30vh]  relative">
                <div class="Line43 2xl:w-[33vw] w-[40vw] h-[0px] 2xl:left-[33vw] left-[39.9vw] top-[14vh] absolute origin-top-left rotate-[180deg] shadow border border-stone-400/opacity-50"></div>
-
-            <div class="BtnKeepRead w-[7vw] h-[5vh] 2xl:left-[26.5vw] left-[33vw] top-[30vh] relative  bg-[#CF9455] rounded-[3px] inline-flex">
+<div class="buttons w-[33vw] h-[8vh]  inline-flex items-end">
+            <div class="BtnKeepRead w-[7vw] h-[5vh] 2xl:left-[26.5vw] left-[33vw] top-[45vh] relative  bg-[#CF9455] rounded-[3px] inline-flex">
                   <div class="Frame273  top-[2.5vh]   justify-center items-center gap-2 inline-flex">
                     <div class=" w-[5.2vw] h-[2.4vh] xl:ml-[0.9em]   text-center text-white 2xl:text-[0.9em] xl:text-[0.75em] font-bold" style="font-family:'El Messiri'">واصل القراءة</div>
                   </div>
 
             </div>
 
-                <div class="Menu w-[13.2vw] h-[7.9vh] top-[27vh] absolute justify-start items-start inline-flex">
+                <div class="Menu w-[13.2vw] h-[7.9vh] top-[45vh] absolute justify-start items-start inline-flex">
                     <div class="DivMkdPostInfoDate w-[4.4vw] h-[7.9vh] relative border border-white">
                     <div class="Copy  left-[1.2vw] top-[1.8vh] h-[7.9vh] justify-center items-center flex "><img class="w-[1.2vw]  h-[auto]" src="{{asset('images/favw.png')}}" alt="fav"></div>
                 </div>
-
+               
             <div class="DivMkdPostInfoCommentsHolder w-[4.4vw] h-[7.9vh] relative border border-gray-200">
               <div class="Comment  left-[0.9vw] top-[1.6vh] h-[7.9vh]   justify-center items-center  flex"><img class="w-[1.5vw]  h-[auto]" src="{{asset('images/like.png')}}" alt="like"></div>
             </div>
@@ -121,13 +121,13 @@
             <div class="AMkdSocialShareDropdownOpener w-[4.4vw] h-[7.9vh] relative border border-white">
               <div class="Share left-[1vw] top-[6vh] h-[7.9vh] justify-center items-center flex"><img class="w-[1.5vw]  h-[auto]" src="{{asset('images/SHARE.png')}}" alt="share"></div>
             </div>
-
+            </div>
           </div>
           <div class="text-container 2xl:w-[33vw] w-[40vw] h-[10.6vh] top-[16vh] absolute text-right text-white 2xl:text-[1.4em] xl:text-[1em] font-normal uppercase leading-normal" style="direction:rtl; font-family:'El Messiri';">
-  <div class="text-preview">
+ <!-- <div class="text-preview">
     المصلحة الشخصية المشروعة المباشرة شرط لقبول الدعوى. مِلاكها أن يكون للمدعي من وراء التجائه إلى القضاء منفعةٌ حقيقة، منفعةٌ لا تقوم بالضرورة بل قضاء. فلا تكون المصلحة قائمة إذا كان ما يسعى إليه قضاءً لن يعود عليه بفائدة حقيقة، بحيث لا يكون في وسعه القانوني أن يضيف إليه حقاً يدعيه، ولا هو بالحتمي لدفع ضرر يتوقع أن يصيبه.
-  </div>
-  <div class="text-full">
+  </div>-->
+  <div class="text-preview">
     المصلحة الشخصية المشروعة المباشرة شرط لقبول الدعوى. مِلاكها أن يكون للمدعي من وراء التجائه إلى القضاء منفعةٌ حقيقة، منفعةٌ لا تقوم بالضرورة بل قضاء. فلا تكون المصلحة قائمة إذا كان ما يسعى إليه قضاءً لن يعود عليه بفائدة حقيقة، بحيث لا يكون في وسعه القانوني أن يضيف إليه حقاً يدعيه، ولا هو بالحتمي لدفع ضرر يتوقع أن يصيبه.
 العقد شريعة المتعاقدِين، وقوته الملزمة تنبع من إرادتهم ترتيبَ أثار محددة عليه، وإذن فمن الطبيعي أن يكون نسبي الآثار، بحيث لا تتعدى الالتزامات والحقوق التي يولدها غيرَ أطرافه.
   </div>
