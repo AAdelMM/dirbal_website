69c4f376756b24a7da3eaba2165f643e1a0a9a8d
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-08-08 07:41:42 +0300

    new toolbar1

--- resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import { TextColorExtension, FontSizeExtension } from './tiptap-extensions';
-
-// تسجيل الامتدادات مع Filament
-window.TextColorPlugin = TextColorExtension;
-window.FontSizePlugin = FontSizeExtension;
