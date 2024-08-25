5bb6d5d464f18f246679bfaf753d3ad95aa9050c
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-08-25 19:48:34 +0300

    update editor and add font sizes

--- config/filament-forms-tinyeditor.php
+++ config/filament-forms-tinyeditor.php
@@ -18,23 +18,66 @@ return [
           'toolbar' => 'fontselect  formatselect fontsizeselect | bold italic | alignjustify alignright aligncenter alignleft | forecolor backcolor | rtl ltr | undo redo removeformat | wordcount fullscreen ',
             'upload_directory' => null,
             'custom_configs' => [
-                'content_css' => [
-                    'https://fonts.googleapis.com/css2?family=Cairo:wght@400..700&display=swap',
-                    'https://fonts.googleapis.com/css2?family=El+Messiri:wght@400..700&display=swap',
-                    'https://fonts.googleapis.com/css2?family=Marhey:wght@300..700&display=swap',
-                    'https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100..900&display=swap',
-                    'https://fonts.googleapis.com/css2?family=Amiri&display=swap', // Adding Amiri font
-                    'https://fonts.googleapis.com/css2?family=Changa:wght@200..700&display=swap', // Adding Changa font
-                ],
-                'font_formats' => 'Arial=arial,helvetica,sans-serif; Times New Roman=times new roman,times,serif; 
-                                   Courier New=courier new,courier,monospace;
-                                   Cairo=cairo, sans-serif; El Messiri=El Messiri, sans-serif; 
-                                   Marhey=Marhey, sans-serif; Noto Kufi Arabic=Noto Kufi Arabic, sans-serif;
-                                   Amiri=amiri, serif; Changa=changa, sans-serif;
-                                   Sakkal Majalla=sakkal majalla, sans-serif;',
+              'content_css' => [
+                'https://fonts.googleapis.com/css2?family=Cairo:wght@400..700&display=swap',
+                'https://fonts.googleapis.com/css2?family=El+Messiri:wght@400..700&display=swap',
+                'https://fonts.googleapis.com/css2?family=Marhey:wght@300..700&display=swap',
+                'https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100..900&display=swap',
+                'https://fonts.googleapis.com/css2?family=Amiri&display=swap',
+                'https://fonts.googleapis.com/css2?family=Changa:wght@200..700&display=swap',
+                'https://fonts.googleapis.com/css2?family=Zain:wght@200;300;400;700;800;900&display=swap',
+                'https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap',
+                'https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap',
+                'https://fonts.googleapis.com/css2?family=Scheherazade+New:wght@400;500;600;700&display=swap',
+            ],
+            'font_formats' => 'Arial=arial,helvetica,sans-serif; Times New Roman=times new roman,times,serif; 
+                               Courier New=courier new,courier,monospace;
+                               Cairo=cairo, sans-serif; El Messiri=El Messiri, sans-serif; 
+                               Marhey=Marhey, sans-serif; Noto Kufi Arabic=Noto Kufi Arabic, sans-serif;
+                               Amiri=amiri, serif; Changa=changa, sans-serif;
+                               Sakkal Majalla=sakkal majalla, sans-serif;
+                               Zain=zain, sans-serif; Almarai=almarai, sans-serif;
+                               Tajawal=tajawal, sans-serif; Scheherazade New=scheherazade new, serif;',
+            'fontsize_formats' => '8pt 10pt 12pt 14pt 16pt 18pt 20pt 22pt 24pt 30pt 36pt 48pt ',
+
+           
+            // Custom styles converted to inline styles
+            'style_formats' => [
+              [
+                  'title' => 'Small Indent & Space',
+                  'block' => 'p',
+                  'styles' => [
+                      'text-indent' => '1em',
+                      'margin-bottom' => '1em',
+                  ],
+                  'wrapper' => false,
+              ],
+              [
+                  'title' => 'Medium Indent & Space',
+                  'block' => 'p',
+                  'styles' => [
+                      'text-indent' => '2em',
+                      'margin-bottom' => '1.5em',
+                  ],
+                  'wrapper' => false,
+              ],
+              [
+                  'title' => 'Large Indent & Space',
+                  'block' => 'p',
+                  'styles' => [
+                      'text-indent' => '3em',
+                      'margin-bottom' => '2em',
+                  ],
+                  'wrapper' => false,
+              ],
+          
             ],
         ],
+        ],
 
+
+
+        
         'simple' => [
             'plugins' => 'autoresize directionality emoticons link wordcount',
             // 'toolbar' => 'removeformat | bold italic | rtl ltr |redo| link emoticons',
