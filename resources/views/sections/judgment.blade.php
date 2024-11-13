18cf8694106981ff5c4759150aabb0a725e52fc2
AAdelMM <ahmed.adel426@gmail.com>
2024-11-13 23:31:25 +0200

    edit kadaa section and pages

--- resources/views/sections/judgment.blade.php
+++ resources/views/sections/judgment.blade.php
@@ -5,7 +5,7 @@
   
   <div class="container mx-auto px-4">
     <div class="Frame285 w-full lg:w-3/4 xl:w-2/3  absolute right-20 top-20 lg:top-28 xl:top-35" style="direction:rtl;">
-      <h1 class="text-white text-[2.25rem] lg:text-[3.75rem] xl:text-[4rem] 2xl:text-[6.5rem] font-normal mb-8 2xl:mb-12" style="font-family:'Noto Kufi Arabic';">القضاء</h1>
+      <h1 class="text-white  text-[2.25rem] lg:text-[3.75rem] xl:text-[4rem] 2xl:text-[6rem] font-normal mb-8 2xl:mb-12" style="font-family:'Noto Kufi Arabic';">القضاء</h1>
         <p class="text-white 2xl:w-[100rem] lg:w-[75rem] mt-[-2rem] text-[1.25rem] lg:text-[1.5rem] 2xl:text-[1.75rem] font-normal leading-relaxed lg:leading-loose" style="font-family:'El Messiri';">
         فهذه مكتبة متواضعة التأمت أجزاؤها من بعض عملي بالمحاكم الدنيا وبالمعهد العالي للقضاء. رجَّحت فائدتها فهأنذا أنشرها.
 القانون الليبي مرجع الأحكام، لكن قدراً غير يسير منها يتوافق مع قوانين دول عربية لاسيما المصرية، المصدر التاريخي لجلّ القوانين العربية.
@@ -14,13 +14,13 @@
     </div>
   </div>
 
-  <div class="SubSection w-full absolute bottom-0 lg:bottom-20 bg-gray-700/20 border-t-2 border-orange-400">
+  <div class="SubSection w-full absolute bottom-0 lg:bottom-10 bg-gray-700/20 border-t-2 border-orange-400 " >
     <div class="container  mx-auto px-4 py-8 2xl:py-16">
       <div class="flex flex-col lg:flex-row justify-around items-center space-y-8 lg:space-y-0 lg:space-x-8 xl:space-x-16">
-        <div class="flex flex-col items-center">
+      <a href="{{ url('/inst') }}"><div class="flex flex-col items-center">
           <img class="w-16 h-16 lg:w-23 lg:h-23 2xl:w-28 2xl:h-28 mb-4" src="{{asset('images/jicon1.png')}}" alt="معهد القضاء" />
           <h2 class="text-gray-200 text-xl lg:text-2xl xl:text-3xl font-bold text-center" style="font-family:'Noto Kufi Arabic'">معهد القضاء</h2>
-        </div>
+        </div></a>
         
         <!-- <img src="{{asset('images/Vline.png')}}" alt="" class="hidden lg:block h-20 xl:h-24"> -->
         
@@ -31,10 +31,10 @@
         
         <img src="{{asset('images/Vline.png')}}" alt="" class="hidden lg:block h-20 xl:h-24">
         
-        <div class="flex flex-col items-center">
+        <a href="{{ url('/court') }}"><div class="flex flex-col items-center">
           <img class="w-16 h-16 lg:w-23 lg:h-23 2xl:w-28 2xl:h-28 mb-4" src="{{asset('images/jicon3.png')}}" alt="النيابة العامة" />
           <h2 class="text-gray-200 text-xl lg:text-2xl xl:text-3xl font-bold text-center" style="font-family:'Noto Kufi Arabic'">المحاكم الدنيا</h2>
-        </div>
+        </div></a>
       </div>
     </div>
   </div>
@@ -62,6 +62,7 @@
           <img class="w-6 h-6 lg:w-8 lg:h-8 cursor-pointer" src="{{ asset('images/magniw.png') }}" alt="search">
           <div class="h-8 border-r border-white"></div>
           <img id="menuIcon3" class="w-6 h-6 lg:w-8 lg:h-8 cursor-pointer" src="{{ asset('images/menuw.png') }}" alt="menu">
+          <div class="h-8 border-r border-white"></div>
         </div>
       </div>
       <div class="border-t border-white mt-4"></div>
