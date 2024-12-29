83730950ed839b8dde1e98638427b4c0c6baed99
AAdelMM <ahmed.adel426@gmail.com>
2024-12-29 16:41:47 +0200

    pdf download2

--- resources/views/previewPages/a7kamShow.blade.php
+++ resources/views/previewPages/a7kamShow.blade.php
@@ -307,7 +307,8 @@ $monthNames = [
             <div id="download_pdf" class="h-[5rem] flex items-center justify-center">
                 <div class="Frame34 flex justify-center gap-[3px] ">
                 @if (!empty($decision->pdf_files))<!-- Ensure $record->pdf_file contains the PDF path -->
-                <a href="{{ asset('storage/pdf_files/' . basename($decision->pdf_files)) }}" download class="btn btn-primary">
+                <a href="{{ asset('storage/pdf_files/' . basename($decision->pdf_files)) }}" 
+                    download="{{ $decision->title ?? 'file' }}.pdf" class="btn btn-primary">
                     
                     <img src="{{ asset('images/pdf.png') }}" alt="download">
                 </a>
